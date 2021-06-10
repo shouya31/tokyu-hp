80a82a7208edb2a51e0f5951de3328b446c19e20
tenpadrummer <yoshiro.kato520@gmail.com>
2021-06-10 20:20:20 +0900

    make resonsive

--- src/views/GreetingPage.vue
+++ src/views/GreetingPage.vue
@@ -1,9 +1,17 @@
 <template>
-    <div class="container-fuild grid grid-cols-4 gap-4">
-        <nav class="px-2 ml-4">
-            <a href="#" class="ml-3"><img src="../assets/tokyu-logo-gray.png" alt="東急百貨店" width="150"></a>
-            <p class="font-weight-900 ml-3 pt-1 tracking-widest">RECRUITMENT</p>
-            <div class="py-48 ml-6">
+    <div class="container-fuild grid grid-cols-1 lg:grid-cols-4 gap-9 lg:gap-7">
+        <!-- sp ヘッダー-->
+        <nav class="flex lg:hidden pl-24 pt-8">
+            <a href="#" class="max-w-xl md:max-w-xs mr-4"><img src="../assets/tokyu-logo-gray.png" alt="東急百貨店"></a>
+            <button class="bg-blue-900 leading-none px-2 ml-auto lg:ml-24 tracking-wide">
+                <p class="text-white text-xs md:text-sm">ENTRY</p>
+            </button>
+        </nav>
+        <!-- sp ヘッダーここまで -->
+        <div class="hidden max-w-full lg:block lg:col-span-1 px-2 ml-4">
+            <a href="#" class="lg:ml-3"><img src="../assets/tokyu-logo-gray.png" alt="東急百貨店" width="150"></a>
+            <p class="hidden lg:block font-weight-900 ml-3 pt-1 tracking-widest">RECRUITMENT</p>
+            <div class="hidden lg:block py-48 ml-6">
                 <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
                     <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"/>
                 </svg>
@@ -11,26 +19,26 @@
             <button class="bg-blue-900 ml-3 mt-28 px-2 tracking-widest">
                 <p class="text-white">ENTRY</p>
             </button>
-        </nav>
-        <div class="col-span-2 pt-20">
-            <img src="../assets/hr-leader.png" class="mt-4" alt="東急百貨店代表" width="710">
+        </div>
+        <div class="col-span-4 p-12 lg:col-span-2 pt-10 lg:pt-16">
+            <img src="../assets/hr-leader.png" class="w-full lg:max-w-3xl mt-4" alt="東急百貨店代表">
             <section class="pb-16 text-gray-700">
-                <h1 class="message text-3xl font-weight-100 tracking-wider pt-9 pb-3">新しい未来に向けて、共に歩んでいきましょう</h1>
+                <h1 class="message text-md font-weight-100 tracking-wider pt-9 pb-3 md:text-3xl">新しい未来に向けて、共に歩んでいきましょう</h1>
                 <hr class="my-3 border-black">
-                <ul class="flex mt-6">
-                    <li class="leader-name"><p class="text-3xl"><span class="mr-4">飯塚</span><span class="ml-4">康浩</span></p></li>
-                    <li><p class="leading-10 ml-4 text-gray-400 tracking-widest">Yasuhiro Iizuka</p></li>
+                <ul class="block mt-6 md:flex">
+                    <li class="leader-name"><p class="text-base md:text-3xl"><span class="mr-4">飯塚</span><span class="ml-4">康浩</span></p></li>
+                    <li><p class="leading-7 ml-0 sm:ml-4 text-gray-400 tracking-widest text-xs sm:leading-10">Yasuhiro Iizuka</p></li>
                 </ul>
-                <p class="mt-3">人事部長</p>
+                <p class="mt-3 text-xs md:text-md">人事部長</p>
             </section>
             <section class="py-8">
                 <!-- Questionのフォント調査 -->
-                <h3 class="text-3xl text-indigo-900">Question</h3>
+                <h3 class="text-md md:text-3xl text-indigo-900">Question</h3>
                 <hr>
-                <h4 class="text-2xl pt-5 font-semibold tracking-wider text-gray-700">求める人物像</h4>
+                <h4 class="text-lg md:text-2xl pt-5 font-semibold tracking-wider text-gray-700">求める人物像</h4>
                 <hr class="border-2 mt-2 mb-4">
-                <p class="bg-white mt-12 p-5 text-2xl font-semibold tracking-widest text-gray-700">A. 企業理念の実践とその一歩先へ</p>
-                <p class="my-7 text-sm tracking-wider leading-8 text-gray-600">
+                <p class="bg-white mt-12 p-5 font-semibold tracking-widest text-gray-700 text-lg md:text-2xl">A. 企業理念の実践とその一歩先へ</p>
+                <p class="my-7 text-xs lg:text-sm tracking-wider leading-8 lg:leading-10 text-gray-600">
                     当社で働くにあたって、その根底にあるのは企業理念の実践です。企業理念にある、
                     「お客様にとって、なくてはならない存在。それが私たちの誇りです」、「誰よりもお客様の近くで、
                     誰よりも深くお客様を思い、誰よりも情熱をもってお客様の力になります」、そして「お客様とともに未来へ向かって
@@ -44,12 +52,12 @@
             </section>
             <section class="py-8">
                 <!-- Questionのフォント調査 -->
-                <h3 class="text-3xl text-indigo-900">Question</h3>
+                <h3 class="text-md md:text-3xl text-indigo-900">Question</h3>
                 <hr>
-                <h4 class="text-2xl pt-5 font-semibold tracking-wider text-gray-700">人事部長として心がけていること</h4>
+                <h4 class="text-lg md:text-2xl pt-5 font-semibold tracking-wider text-gray-700">人事部長として心がけていること</h4>
                 <hr class="border-2 mt-2 mb-4">
-                <p class="bg-white mt-12 p-5 text-2xl font-semibold tracking-widest text-gray-700">A. 「人材」を「人財」へと進化させていくために</p>
-                <p class="my-7 text-sm tracking-wider leading-8 text-gray-600">
+                <p class="bg-white mt-12 p-5 font-semibold tracking-widest text-gray-700 text-lg md:text-2xl">A. 「人材」を「人財」へと進化させていくために</p>
+                <p class="my-7 text-xs lg:text-sm tracking-wider leading-8 lg:leading-10 text-gray-600">
                     普段から心がけているのは、「否定をしないこと」と「メンバーの様子に気を配ること」。共に働く仲間とその
                     一人ひとりが持つ熱意を大切にしたい......。人事部長として、それぞれの個性を活かし、尊重する人材育成を行って
                     いきたいと考えています。組織を城の城壁に例えると、城壁は様々な形とおおきさの石垣が重なり合い、崩れない
@@ -58,14 +66,14 @@
                     今と未来を支えていくことが自分の役目であると考えています。
                 </p>
             </section>
-            <section class="pt-8 pb-32">
+            <section class="pt-8 pb-8 lg:pb-32">
                 <!-- Questionのフォント調査 -->
-                <h3 class="text-3xl text-indigo-900">Question</h3>
+                <h3 class="text-md md:text-3xl text-indigo-900">Question</h3>
                 <hr>
-                <h4 class="text-2xl pt-5 font-semibold tracking-wider text-gray-700">応募者へのメッセージ</h4>
+                <h4 class="text-lg md:text-2xl pt-5 font-semibold tracking-wider text-gray-700">応募者へのメッセージ</h4>
                 <hr class="border-2 mt-2 mb-4">
-                <p class="bg-white mt-12 p-5 text-2xl font-semibold tracking-widest text-gray-700">A. 共に歩み、成長する、未来へ向かって</p>
-                <p class="my-7 text-sm pb-5 tracking-wider leading-8 text-gray-600">
+                <p class="bg-white mt-12 p-5 font-semibold tracking-widest text-gray-700 text-lg md:text-2xl">A. 共に歩み、成長する、未来へ向かって</p>
+                <p class="my-7 text-xs lg:text-sm tracking-wider leading-8 lg:leading-10 text-gray-600">
                     ご存知の通り、百貨店業界を取り巻く環境は決して楽観できるものではなく、構造改革や新たな事業モデル構築の
                     動きが加速しています。そうした不透明な状況下でも、当社は「融合型リテーラー（多事業戦略）」の実現に向け、
                     より強固な事業構造への転換と*DXの推進による新しいお買い物体験の提供を進めています。今後、いかにその推進速度を
@@ -75,7 +83,7 @@
                     そして行動力が必要です。企業理念を実践し続け、お客様の豊かで上質な暮らしづくりに貢献していきましょう。
                     共に歩み、成長する、未来へ向かって。ご応募お待ちしております。
                 </p>
-                <p class="text-sm"><small class="text-gray-600">
+                <p class="text-xs lg:text-sm"><small class="text-gray-600">
                     *DX: Digital Transformationの略。デジタル技術を浸透させることで
                     人々の生活をより良いものへと<br>変革すること。また、既存の価値観を覆す革新的なイノベーションをもたらすもの。
                 </small></p>
@@ -84,8 +92,8 @@
         <footer class="col-span-4 bg-white pt-14 pb-10 text-center">
             <!-- 画像ちょっと横にずれてる？ -->
             <a href=""><img src="../assets/tokyu-logo-white.png" alt="東急百貨店" width="180" class="mx-auto bg-white"></a>
-            <p class="recruitment text-black mb-5">RECRUITMENT</p>
-            <p class="text-sm mt-5 italic"><small>Copyright © TOKYU DEPARTMENT STORE CO.,LTD All Rights Reserved.</small></p>
+            <p class="text-sm md:text-base recruitment text-black mt-6 lg:my-6">RECRUITMENT</p>
+            <p class="text-xs lg:text-sm mt-5 italic"><small>Copyright © TOKYU DEPARTMENT STORE CO.,LTD All Rights Reserved.</small></p>
         </footer>
     </div>
 

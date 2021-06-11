c7f20458cecac422703b289d6344d87316b50949
higuchi shouya <higuchishouya@MacBook-Pro-2.local>
2021-06-11 23:16:36 +0900

    スライドショー実装

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,17 +24,19 @@
                 </div>
             </div>
 
-            <section class="fv w-full  hidden lg:block">
+            <section id="fv-main" class="fv w-full  hidden lg:block">
                 <div class="fv-header flex p-10 m-8">
                     <p class="relative text-5xl font-thin tracking-wider" style="left: -20px; font-family: serif;">共に歩む</p>
                     <p class="relative text-4xl tracking-wider top-6" style="font-family top: 20px; left: -10px; font-size: 60px;">×</p>
                     <p class="relative text-6xl tracking-widest" style="font-family: emoji; font-weight: 600; right: -10px;bottom: -40px; color: #003366;">VISION</p>
                 </div>
-                <div class="fv-contents flex">
-                    <div class="fv-left-content w-11/12">
-                        <img src="https://i.pinimg.com/originals/95/35/ef/9535efedcf312e1b90845f27a7e815c8.jpg">
-                    </div>
-
+                <div class="fv-contents flex" v-on:click="show = !show">
+                    <transition name="fade">
+                        <div v-if="show" class="fv-left-content w-11/12">
+                            <img :src="images[index]" :key="images[index]" alt="" width="800" height="550" style="height:550px;">
+                        </div>
+                    </transition>
+         
                     <div class="fv-right-content absolute right-0">
                         <div class="flex justify-end m-10">
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0-VliuBzfhMkFESbIx1mThj2Ay9Ll-UnYhQ&usqp=CAU" alt="" class="relative w-3/12" style="top: 170px;left: 65px; height: 180px;">
@@ -78,12 +80,34 @@
 </template>
 
 <script>
-// ここにJSを実装する
+export default{
+  data() {
+    return{
+        show: true,
+        index: 0,
+        images: [
+            "https://i.pinimg.com/originals/95/35/ef/9535efedcf312e1b90845f27a7e815c8.jpg",
+            "https://contents.oricon.co.jp/special/img/55000/55338/detail/img660/1603497089912.jpg",
+            "https://www.bunkamura.co.jp/museum/exhibition/17_kyosai/topics/assets_c/2017/03/20170306165143-thumb-500x345-17692.jpg"
+        ]
+    }
+  },
+   mounted() {
+    setInterval(() => {
+        this.index = this.index < this.images.length - 1 ? this.index + 1 : 0;
+    }, 4000);
+  },
+}
 
 </script>
 
 <style>
-/* ここにCSSを実装する */
 @import "../assets/style/home.css";
 
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
 </style>

d027d89ac7f6a21d65a3a12f79761eb83f4e8300
tenpadrummer <yoshiro.kato520@gmail.com>
2021-06-18 01:23:35 +0900

    coding People_page resposive_sp

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="fixed sidebar col-span-1">
+  <div class="lg:fixed sidebar lg:w-2/12 lg:col-span-1">
     <!-- sp ヘッダー-->
-    <nav class="flex hidden pl-24 pt-8 mx-auto">
-      <a href="/" class="max-w-xl md:max-w-xs"
-        ><img src="../assets/tokyu-logo.png" alt="東急百貨店"
-      /></a>
+    <nav class="h-32 lg:hidden">
+      <div class="left-nav-content float-left ml-3 w-40">
+        <a href="/" class="w-3 md:max-w-xs"><img src="../assets/tokyu-logo.png" alt="東急百貨店"/></a>
+        <p class="font-weight-900 ml-3 pt-1 tracking-widest">RECRUITMENT</p>
+      </div>
+      <div class="right-nav-content float-right pt-8">
+        <div class="menu h-20 w-20">
+          <p><span style="width: 30%;"></span></p>
+          <p><span style="width: 30%;"></span></p>
+        </div>
+      </div>
     </nav>
     <!-- sp ヘッダーここまで -->
     <div class="hidden max-w-full lg:block lg:col-span-1 p-8">

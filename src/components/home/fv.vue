8fd233bc79726d77756efa9387b75f60df15a993
higuchi shouya <higuchishouya@MacBook-Pro-2.local>
2021-06-27 23:03:39 +0900

    fv-imageアニメーション実装

--- src/components/home/fv.vue
+++ src/components/home/fv.vue
@@ -1,49 +1,68 @@
 <template>
-<div>
-  <div id="fv-main" class="fv w-full pl-60 mb-20 hidden lg:block">
-    <div class="fv-header flex m-8 pt-10">
-      <img src="../../assets/images/fv-vision.png">
-    </div>
-    <div class="fv-contents flex">
-      <div class="fv-left-content w-11/12" style="z-index:-3;">
-        <transition name="fade" mode="out-in">
-          <img
-            :src="images[index]"
-            :key="images[index]"
-            alt=""
-            width="800"
-            height="550"
-            style="height:550px; z-index:-3;"
-          />
-        </transition>
+  <div>
+    <div id="fv-main" class="fv w-full pl-60 mb-20 hidden lg:block">
+      <div class="fv-header flex m-8 pt-10">
+        <img src="../../assets/images/fv-vision.png" />
       </div>
-      <div class="fv-right-content absolute right-0">
-        <div class="flex justify-end m-10">
-          <img
-            src="../../assets/images/home-fv2.png"
-            alt=""
-            class="relative w-3/12"
-            style="top: 170px;left: 65px; width: 13vw; z-index:-2;"
-          />
-          <img
-            src="../../assets/images/home-fv1.png"
-            alt=""
-            class="relative w-3/12"
-            style="top: -140px; left: 30px; width: 13vw; z-index:-2;"
-          />
-          <img
-            src="../../assets/images/home-fv3.png"
-            alt=""
-            class="relative w-3/12"
-            style="bottom: -280px; width: 13vw; z-index:-2;"
-          />
+      <div class="fv-contents flex">
+        <div class="fv-left-content w-11/12" style="z-index:-3;">
+          <transition name="fade" mode="out-in">
+            <img
+              :src="images[index]"
+              :key="images[index]"
+              alt=""
+              width="800"
+              height="550"
+              style="height:550px; z-index:-3;"
+            />
+          </transition>
+        </div>
+        <div class="fv-right-content absolute right-0 fv-person-animation">
+          <div class="flex justify-end m-10">
+            <img
+              src="../../assets/images/home-fv2.png"
+              alt=""
+              class="relative w-3/12"
+              style="top: 170px;left: 65px; width: 13vw; z-index:-2;;"
+            />
+            <img
+              src="../../assets/images/home-fv1.png"
+              alt=""
+              class="relative w-3/12"
+              style="top: -140px; left: 30px; width: 13vw; z-index:-2;;"
+            />
+            <img
+              src="../../assets/images/home-fv3.png"
+              alt=""
+              class="relative w-3/12"
+              style="bottom: -280px; width: 13vw; z-index:-2;;"
+            />
+          </div>
+          <div class="flex justify-end mt-96">
+            <img
+              src="../../assets/images/home-fv5.png"
+              alt=""
+              class="relative w-3/12"
+              style="top: 280px;left: 65px; width: 13vw; z-index:-2;;"
+            />
+            <img
+              src="../../assets/images/home-fv4.png"
+              alt=""
+              class="relative w-3/12"
+              style="left: 30px; width: 13vw; z-index:-2;;"
+            />
+            <img
+              src="../../assets/images/home-fv6.png"
+              alt=""
+              class="relative w-3/12"
+              style="bottom: -420px; width: 13vw; z-index:-2;;"
+            />
+          </div>
         </div>
       </div>
     </div>
+    <FvSp />
   </div>
-  <FvSp />
-</div>
-
 </template>
 
 <script>
@@ -51,9 +70,9 @@ import FvSp from "./fv-sp.vue";
 
 export default {
   name: "HomeFvSection",
-   components: {
+  components: {
     FvSp,
-   },
+  },
   data() {
     return {
       show: true,
@@ -73,11 +92,27 @@ export default {
 </script>
 
 <style>
-.font-BrandonGrotesque{
+.fv-person-animation {
+  animation: person_moveY 12s linear 0s infinite;
+  z-index: -2;
+}
+@keyframes person_moveY {
+  0% {
+    transform: scaleY(1) translateY(500px);
+  }
+  50% {
+
+  }
+  100% {
+    transform: scaleY(1) translateY(-1200px);
+  }
+}
+
+.font-BrandonGrotesque {
   font-family: Brandon Grotesque;
 }
-.font-NotoSerif{
-  font-family: 'Noto Serif JP', serif;
+.font-NotoSerif {
+  font-family: "Noto Serif JP", serif;
 }
 .fade-enter-active,
 .fade-leave-active {
@@ -86,13 +121,13 @@ export default {
 .fade-enter,
 .fade-leave-to {
   opacity: 0;
-  z-index:-3;
+  z-index: -3;
 }
 .explain-text {
   font-size: 18px;
-  font-family: 'Noto Sans Japanese',sans-serif;
+  font-family: "Noto Sans Japanese", sans-serif;
   line-height: 32px;
-  z-index:-3;
+  z-index: -3;
   font-weight: 300;
 }
 
@@ -107,4 +142,3 @@ export default {
   }
 }
 </style>
-

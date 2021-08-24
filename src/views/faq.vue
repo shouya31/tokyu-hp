5c4b0c87d8503059a72d099649611c99395ccfd9
栗田ひかり <49625998+kuritahikari@users.noreply.github.com>
2021-08-25 05:48:57 +0900

    FAQページ・募集要項ページ
    見た目のみ実装
    
    以下未着手
    ・サイドバーが表示されない（原因未解明）
    ・テキスト流し込み
    ・FAQ、アコーディオンのアニメーション
    ・フォントの詳細な指定（letter-spacingとfont-family）

--- src/views/faq.vue
+++ src/views/faq.vue
@@ -1,11 +1,168 @@
 <template>
-  <div>
-    <!-- HTMLの記述 -->
-    <h2>FAQ</h2>
+<div class="faq overflow-hidden">
+ 
+    <section class="">
+      <div class="flex justify-between">
+      <div class="">
+        <div class="relative lg:right-12">
+          <img
+            src="@/assets/images/recruit-title.png"
+            alt=""
+            class="lg:w-full people-logo-sp"
+          />
+        </div>
+      </div>
+      <div class="current-nav">
+        <p>FAQ</p>
+      </div>
+      </div>
+      <h2 class="mx-auto">
+        <img src="@/assets/images/faq-title.png" alt="Frequently Asked Questions">
+      </h2>
+    </section>
+
+    <section class="faq-contents mx-auto">
+      <h3 class="text-center">採用・配属について</h3>
+      <div class="">
+        <dl class="listQA">
+          <dt>
+            <p>どのような人材を求めていますか?</p>
+          </dt>
+          <dd>
+            <p>「既成概念に捉われず、変革思考と柔軟な発想を持つ人材」、「主体的にアイデアを創出、具現化し、結果を出していくことができる人材」を求めています。</p>
+          </dd>
+        </dl>
+        <dl class="listQA">
+          <dt>
+            <p>選考ステップを教えてください。</p>
+          </dt>
+          <dd>
+            <p>書類選考、適性検査、面接（複数回）を実施いたします。</p>
+          </dd>
+        </dl>
+        <dl class="listQA">
+          <dt>
+            <p>入社前に必要な技能や資格があれば教えてください。?</p>
+          </dt>
+          <dd>
+            <p>特にございません。入社後、業務上必要な資格については、会社が取得費用を補助いたします。</p>
+          </dd>
+        </dl>
+        <dl class="listQA">
+          <dt>
+            <p>地域別の採用は行っていますか?</p>
+          </dt>
+          <dd>
+            <p>東京地区採用と北海道地区採用に分かれており、選考フローも異なります。原則、東京地区にて採用された方は東京・神奈川の事業所、北海道地区で採用された方は北海道が勤務地になります。<br>
+              ※詳細は募集要項をご確認ください。</p>
+          </dd>
+        </dl>
+        <dl class="listQA">
+          <dt>
+            <p>短期大学、専門学校卒の採用は行っていますか?</p>
+          </dt>
+          <dd>
+            <p>現在、行っておりません。</p>
+          </dd>
+        </dl>
+        <dl class="listQA">
+          <dt>
+            <p>入社後の配属について教えてください。</p>
+          </dt>
+          <dd>
+            <p>総合職として様々な職場に配属される可能性があります。<br>
+                また、人材育成の視点から、最初の配属後も複数の職場・職種を経験していただき、個々人の自発的なキャリア形成を促進しています。</p>
+          </dd>
+        </dl> 
+      </div>
+    </section>
+
+
+
+
+    <footer class="col-span-12 bg-white pt-14 pb-10 text-center">
+      <a href="/"
+        ><img
+          src="@/assets/images/footer-logo.png"
+          alt="東急百貨店"
+          width="180"
+          class="mx-auto bg-white"
+      /></a>
+      <p class="text-xs lg:text-sm mt-5 italic">
+        <small
+          >Copyright © TOKYU DEPARTMENT STORE CO.,LTD All Rights
+          Reserved.</small
+        >
+      </p>
+    </footer>
   </div>
 </template>
 
 <style>
-/* CSSの記述 */
-
+.faq {
+  background-color: rgba(213,213,213,.3);
+}
+.current-nav {
+  width: 204px;
+  height: 35px;
+  border: solid 1px #707070;
+  text-align: center;
+  line-height: 35px;
+  font-weight: bold;
+  margin-right: 86px;
+}
+.faq h2 {
+  width: fit-content;
+  margin-top: 105px;
+}
+.faq h3 {
+  margin-top: 70px;
+  margin-bottom: 65px;
+  font-size: 20px;
+  font-family: 'Noto Serif JP', serif;
+}
+.faq .faq-contents {
+  max-width: 728px;
+  margin-bottom: 80px;
+}
+.faq .listQA {
+  width: 100%;
+  padding: 24px;
+}
+.faq dl {
+    background-color: #FFF;
+    margin-bottom: 10px;
+}
+.faq .listQA dt {
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 44px;
+  display: flex;
+  position: relative;
+}
+.faq .listQA dt::before {
+  content: "Q";
+  margin-right: 20px;
+}
+.faq .listQA dt::after {
+  content: "";
+  border-bottom: solid 1px #1E344D;
+  width: 22px;
+  position: absolute;
+  right: 1%;
+}
+/* .faq .listQA dt::after {
+  content: "";
+  border-bottom: solid 1px #1E344D;
+  border-right: solid 1px #1E344D;
+  width: 18px;
+  height: 18px;
+  transform: rotate(45deg);
+  position: absolute;
+  right: 1%;
+} */
+.faq .listQA dd {
+  font-size: 14px;
+  font-weight: normal;
+}
 </style>
--- src/views/recruit.vue
+++ src/views/recruit.vue
@@ -1,11 +1,165 @@
 <template>
-  <div>
-    <!-- HTMLの記述 -->
-    <h2>Recruit</h2>
+  <div class="recruit-requirement overflow-hidden">
+    <section class="">
+      <div class="flex justify-between">
+        <div class="">
+          <div class="relative lg:right-12">
+            <img
+              src="@/assets/images/recruit-title.png"
+              alt=""
+              class="lg:w-full people-logo-sp"
+            />
+          </div>
+        </div>
+        <div class="current-nav">
+          <p>募集要項&企業概要</p>
+        </div>
+      </div>
+    </section>
+    <section class="recruit-requirement-content mx-auto">
+      <h2 class="mx-auto">
+        <img
+          src="@/assets/images/requirement-title.png"
+          alt="Application Requirements"
+        />
+      </h2>
+      <h3 class="text-center">募集要項</h3>
+      <div class="info">
+        <dl>
+          <dt>採用人数</dt>
+          <dd>
+            2022年 5名（予定）<br />
+            2021年 13名<br />
+            2020年 17名<br />
+            2019年 33名<br />
+            2018年 22名
+          </dd>
+          <dt>募集職種</dt>
+          <dd>総合職</dd>
+          <dt>募集学部・学科</dt>
+          <dd>全学部・全学科</dd>
+          <dt>初任給</dt>
+          <dd>
+            （2021年度実績）<br />
+            東京地区：月給210,000円<br />
+            北海道地区：月給199,500円
+          </dd>
+          <dt>昇給・賞与</dt>
+          <dd>昇給 年１回 賞与 年２回</dd>
+          <dt>退職金制度</dt>
+          <dd>有</dd>
+        </dl>
+      </div>
+    </section>
+    <section class="recruit-requirement-content mx-auto">
+      <h2 class="mx-auto">
+        <img
+          src="@/assets/images/company-overview-title.png"
+          alt="Frequently Asked Questions"
+        />
+      </h2>
+      <h3 class="text-center">企業概要</h3>
+            <div class="info">
+        <dl>
+          <dt>ダミーダミー</dt>
+          <dd>
+            2022年 5名（予定）<br />
+            2021年 13名<br />
+            2020年 17名<br />
+            2019年 33名<br />
+            2018年 22名
+          </dd>
+          <dt>ダミーダミー</dt>
+          <dd>総合職</dd>
+          <dt>ダミーダミー</dt>
+          <dd>全学部・全学科</dd>
+          <dt>ダミーダミー</dt>
+          <dd>
+            （2021年度実績）<br />
+            東京地区：月給210,000円<br />
+            北海道地区：月給199,500円
+          </dd>
+          <dt>ダミーダミー</dt>
+          <dd>昇給 年１回 賞与 年２回</dd>
+          <dt>ダミーダミー</dt>
+          <dd>有</dd>
+        </dl>
+      </div>
+    </section>
+ 
+
+    <footer class="col-span-12 bg-white pt-14 pb-10 text-center">
+      <a href="/"
+        ><img
+          src="@/assets/images/footer-logo.png"
+          alt="東急百貨店"
+          width="180"
+          class="mx-auto bg-white"
+      /></a>
+      <p class="text-xs lg:text-sm mt-5 italic">
+        <small
+          >Copyright © TOKYU DEPARTMENT STORE CO.,LTD All Rights
+          Reserved.</small
+        >
+      </p>
+    </footer>
   </div>
 </template>
 
 <style>
-/* CSSの記述 */
-
+.recruit-requirement {
+  background-color: rgba(213, 213, 213, 0.3);
+}
+.recruit-requirement .recruit-requirement-content {
+  max-width: 728px;
+  margin-bottom: 80px;
+}
+.current-nav {
+  width: 204px;
+  height: 35px;
+  border: solid 1px #707070;
+  text-align: center;
+  line-height: 35px;
+  font-weight: bold;
+  margin-right: 86px;
+}
+.recruit-requirement h2 {
+  width: fit-content;
+  margin-top: 105px;
+}
+.recruit-requirement h3 {
+  margin-top: 70px;
+  margin-bottom: 65px;
+  font-size: 20px;
+  font-family: "Noto Serif JP", serif;
+}
+.recruit-requirement .info {
+  width: 100%;
+}
+.recruit-requirement .info dl {
+display: flex;
+flex-flow: row wrap;
+width: 100%;
+border-top: 1px solid #16344F;
+}
+.recruit-requirement .info dt {
+color: #1B224C;
+font-size: 16px;
+font-weight: bold;
+flex-basis: 25%;
+padding: 20px 24px;
+background-color: rgba(27,34,76,.3);
+border-bottom: 1px solid #16344F;
+}
+.recruit-requirement .info dd {
+flex-basis: 75%;
+padding: 20px 24px;
+background-color: #fff;
+border-bottom: 1px solid #16344F;
+}
+@media screen and (max-width: 559px) {
+.recruit-requirement .info dl {
+flex-flow: column;
+}
+}
 </style>

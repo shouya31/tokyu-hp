79bdf3d6bd0eb98a662afd7fb75657cf9eb0bb81
栗田ひかり <49625998+kuritahikari@users.noreply.github.com>
2021-09-05 04:34:43 +0900

    TOPページを中心にフロント修正

--- src/components/home/footer.vue
+++ src/components/home/footer.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <div class="to-pagetop">
+      <a href="!#">
+        <img src="../../assets/images/pagetop-btn.svg" alt="PAGETOP">
+      </a>
+
+    </div>
     <footer class="bg-white text-center mt-30 lg:block hidden">
       <a href=""
         ><img
